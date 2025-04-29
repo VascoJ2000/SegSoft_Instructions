654476a58f710724763ac1a2d5c2302a1c353e98
VascoJ2000 <63782112+VascoJ2000@users.noreply.github.com>
2025-04-29 19:34:12 +0100

    Update

---
+++ README.md
@@ -0,0 +1,58 @@
+# 70140_70481 Software Security Project Guide
+
+## Working with the Server
+
+### 0. Endpoints
+
+#### 0.1 /api/auth/register
+
+***Request***
+```
+
+```
+
+#### 0.2 /Oauth/register
+
+***Request***
+```
+
+```
+
+#### 0.3 /Oauth/authorize
+
+***Request***
+```
+
+```
+
+#### 0.4 /Oauth/token
+
+***Request***
+```
+
+```
+
+### 1. Register you Client
+
+#### 1.1 OPTIONAL - Register an account (just username and password)
+
+### 2. Login
+
+### 3. Receive Authorization code
+
+### 4. Authenticate and receive back token
+
+
+## Working with the client
+
+### 0. Options
+
+#### 0.1. Change your Client ID
+
+#### 0.2. Change your Secret
+
+#### 0.3. Change your Server
+
+### 1. Login
+
+### 2. Results
---
+++ index.js
@@ -0,0 +1,89 @@
+const url = window.location;
+const secret = 'CB2C7B60537C6F19CA265A93D52C3B779401807D07FA77B921DC91B2BBF118BE';
+const clientId = '70140_70481';
+const authServer = 'http://localhost:4000';
+
+// --- Configuration ---
+const redirectUri = url;
+const responseType = 'code';
+const scope = 'default';
+const state = Math.random().toString(36).substring(2); // Random string for security
+
+document.getElementById('ourOauth').onclick = function() {
+  const authEndpoint = authServer + '/Oauth/authorize';
+  const authUrl = `${authEndpoint}?response_type=${responseType}&client_id=${clientId}&scope=${encodeURIComponent(scope)}&state=${state}`;
+  window.location.href = authUrl;
+};
+
+document.getElementById('70665-70715_Oauth').onclick = function() {
+  const authEndpoint = 'https://authorization-server-70665-70715.onrender.com/Oauth/authorize';
+  const authUrl = `${authEndpoint}?response_type=${responseType}&client_id=${clientId}&scope=${encodeURIComponent(scope)}&state=${state}`;
+  window.location.href = authUrl;
+};
+
+document.getElementById('clientBtn').onclick = async function() {
+  const response = await fetch(authServer + '/Oauth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: clientId,
+        secret: secret,
+        redirectUri: redirectUri.href,
+      })
+  });
+
+  const data = await response.json();
+  console.log(data);
+};
+
+document.getElementById('registerBtn').onclick = async function() {
+  const username = document.getElementById('RegisterUsername').value;
+  const password = document.getElementById('RegisterPassword').value;
+
+  if(username && password){
+    const response = await fetch(authServer + '/api/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+  });
+
+  const data = await response.json();
+  console.log(data);
+  }else{
+    alert("Username or password missing!!!")
+  }
+}
+
+// Handle redirect back with the code
+window.onload = async function() {
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get('code');
+  const returnedState = params.get('state');
+
+  if (code) {
+    document.getElementById('result').innerText = `Received Authorization Code: ${code}`;
+
+    // Exchange authorization code for access token
+    const response = await fetch(`${authServer}/Oauth/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        client_id: clientId,
+      })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    document.getElementById('result').innerText += `\nAccess Token: ${data.token}`;
+  }else{
+    console.log("No code");
+  }
+};
